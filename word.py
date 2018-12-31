7df3f6b71321c6abe5b4d800c005c5179c3f96b2
star cat <44723961+starcat520@users.noreply.github.com>
2018-12-31 15:01:17 +0800

    Update word.py

--- word.py
+++ word.py
@@ -3,6 +3,6 @@ import requests,sys
 if sys.getdefaultencoding() != 'utf-8':
    reload(sys)
    sys.setdefaultencoding('utf-8')
-r = requests.get('http://www.xiazai520.xyz/wordjs')
+r = requests.get('http://www.freeyuming.cn/wordjs')
 print r.text
 				
