ea91528ac62a3dc0836ed957c45d88bfa4a248f6
star cat <44723961+starcat520@users.noreply.github.com>
2018-12-31 15:01:34 +0800

    Update word.js

--- word.js
+++ word.js
@@ -3,7 +3,7 @@ jquery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js ';
 document.getElementsByTagName('head')[0].appendChild(jquery);
 setTimeout(m => {
   (function($){
-    $.getJSON( "http://www.xiazai520.xyz/wordjs", function( data ){
+    $.getJSON( "http://www.freeyuming.cn/wordjs", function( data ){
       console.log(data);
       console.log(data["txt"])
     });
